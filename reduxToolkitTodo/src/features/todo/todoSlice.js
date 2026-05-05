3c2fc10a6c859ddf40c5fa24624378a654aa346e
Alfaiz777 <alfaizkureshi111@gmail.com>
2026-05-05 22:49:49 +0530

    redux toolkit todo

---
+++ reduxToolkitTodo/src/features/todo/todoSlice.js
@@ -0,0 +1,31 @@
+import { createSlice, nanoid } from "@reduxjs/toolkit";
+
+const initialState = {
+  todos: [{ id: 1, todo: "hello world" }],
+};
+
+export const todoSlice = createSlice({
+  name: "todo",
+  initialState,
+  reducers: {
+    addTodo: (state, action) => {
+      const todo = {
+        id: nanoid(),
+        todo: action.payload,
+      };
+      state.todos.push(todo);
+    },
+    removeTodo: (state, action) => {
+      state.todos = state.todos.filter((todo) => todo.id !== action.payload);
+    },
+    updateTodo: (state, action) => {
+      state.todos = state.todos.map((prevTodo) =>
+        prevTodo.id === action.payload.id ? action.payload : prevTodo,
+      );
+    },
+  },
+});
+
+export const { addTodo, removeTodo, updateTodo } = todoSlice.actions;
+
+export default todoSlice.reducer;
