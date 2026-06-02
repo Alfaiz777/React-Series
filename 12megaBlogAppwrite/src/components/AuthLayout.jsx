1f71dd4fb41ac22d8ac58dd74ccf45c5ed529a65
Alfaiz777 <alfaizkureshi111@gmail.com>
2026-06-02 15:43:41 +0530

    AuthLayout setLoader bug solved.

--- 12megaBlogAppwrite/src/components/AuthLayout.jsx
+++ 12megaBlogAppwrite/src/components/AuthLayout.jsx
@@ -1,10 +1,9 @@
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 import { useSelector } from "react-redux";
 
 export default function Protected({ children, authentication = true }) {
   const navigate = useNavigate();
-  const [loader, setLoader] = useState(true);
   const authStatus = useSelector((state) => state.auth.status);
 
   useEffect(() => {
@@ -14,8 +13,7 @@ export default function Protected({ children, authentication = true }) {
     } else if (!authentication && authStatus !== authentication) {
       navigate("/");
     }
-    setLoader(false);
   }, [authStatus, navigate, authentication]);
 
-  return loader ? <h1>Loading...</h1> : <>{children}</>;
+  return <>{children}</>;
 }
