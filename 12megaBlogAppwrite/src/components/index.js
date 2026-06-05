ad474c3dc31ccd8d7ae2db427647e0b68bdc226a
Alfaiz777 <alfaizkureshi111@gmail.com>
2026-06-05 22:40:01 +0530

    created postForm component

--- 12megaBlogAppwrite/src/components/index.js
+++ 12megaBlogAppwrite/src/components/index.js
@@ -5,5 +5,25 @@ import Logo from "./Logo";
 import LogoutBtn from "./Header/LogoutBtn";
 import Button from "./Button";
 import Input from "./Input";
+import SelectButton from "./SelectButton";
+import AuthLayout from "./AuthLayout";
+import Login from "./Login";
+import PostCard from "./PostCard";
+import RTE from "./RTE";
+import SignUp from "./SignUp";
 
-export { Header, Footer, Container, Logo, LogoutBtn, Button, Input };
+export {
+  Header,
+  Footer,
+  Container,
+  Logo,
+  LogoutBtn,
+  Button,
+  Input,
+  SelectButton,
+  AuthLayout,
+  Login,
+  PostCard,
+  RTE,
+  SignUp,
+};
