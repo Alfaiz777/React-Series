32e2a4e5979798febec5c07828570a4ba7aeacdc
Alfaiz777 <alfaizkureshi111@gmail.com>
2026-06-10 18:19:33 +0530

    All post page bug resolved

--- 12megaBlogAppwrite/src/pages/AllPosts.jsx
+++ 12megaBlogAppwrite/src/pages/AllPosts.jsx
@@ -4,13 +4,13 @@ import { Container, PostCard } from "../components";
 
 function AllPosts() {
   const [posts, setPosts] = useState([]);
-  useEffect(() => {}, []);
-
-  appWriteService.getPosts([]).then((posts) => {
-    if (posts) {
-      setPosts(posts.documents);
-    }
-  });
+  useEffect(() => {
+    appWriteService.getPosts([]).then((posts) => {
+      if (posts) {
+        setPosts(posts.documents);
+      }
+    });
+  }, []);
 
   return (
     <div className="w-full py-8">
@@ -18,7 +18,7 @@ function AllPosts() {
         <div className="flex flex-wrap">
           {posts.map((post) => (
             <div key={post.$id} className="p-2 w-1/4">
-              <PostCard post={post} />
+              <PostCard {...post} />
             </div>
           ))}
         </div>
